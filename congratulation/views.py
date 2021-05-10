783c5c7bbe87476543b729d627ad58a475d847cc
acetone415 <acetone415@gmail.com>
2021-05-10 17:29:32 +0300

    Add urls.py in congradulation

--- congratulation/views.py
+++ congratulation/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("Soon you can congratulate here)")
 
 # Create your views here.
