fea698f667d4473ce89efe0ffbbd7ab54df09cbf
acetone415 <acetone415@gmail.com>
2021-05-13 16:57:26 +0300

    Add models in congratulation

--- congratulation/models.py
+++ congratulation/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название песни')
+    author = models.CharField(max_length=50, verbose_name='Автор')
+
+
+class Order(models.Model):
+    song = models.ForeignKey('Song')
+    congratulation = models.TextField()
+    datetime = models.DateTimeField()
