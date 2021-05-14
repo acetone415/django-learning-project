8c8d4a70ee497cb5a6bdcfe6b17c09033266aaa2
acetone415 <acetone415@gmail.com>
2021-05-14 15:47:17 +0300

    Update models.py and admin.py in congratulation

--- congratulation/admin.py
+++ congratulation/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Song, Order
 
-# Register your models here.
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title')
+    list_display_links = ('author', 'title')
+    search_fields = ('author', 'title')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('song', 'congratulation')
+    list_display_links = ('congratulation',)
+
+
+admin.site.register(Song, SongAdmin)
+admin.site.register(Order, OrderAdmin)
--- congratulation/models.py
+++ congratulation/models.py
@@ -3,10 +3,27 @@ from django.db import models
 
 class Song(models.Model):
     title = models.CharField(max_length=50, verbose_name='Название песни')
-    author = models.CharField(max_length=50, verbose_name='Автор')
+    author = models.CharField(max_length=50, verbose_name='Автор',
+                              db_index=True)
+
+    def __str__(self):
+        return f'{self.author} - {self.title}'
+
+    class Meta:
+        verbose_name = 'Песня'
+        verbose_name_plural = 'Песни'
+        ordering = ['author', 'title']
 
 
 class Order(models.Model):
-    song = models.ForeignKey('Song', on_delete=models.PROTECT)
-    congratulation = models.TextField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    song = models.ForeignKey('Song', on_delete=models.PROTECT,
+                             verbose_name='Песня')
+    congratulation = models.TextField(null=True, blank=True,
+                                      verbose_name='Текст поздравления')
+    published = models.DateTimeField(auto_now_add=True, db_index=True,
+                                     verbose_name='Текст поздравления')
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        ordering = ['-published']
