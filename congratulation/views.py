aaf435fd6c4dc0cf0b1f9d1ec77fa2f5ab933423
acetone415 <acetone415@gmail.com>
2021-06-29 20:33:51 +0300

    replace templates to <app>/templates/<app> folder

--- congratulation/views.py
+++ congratulation/views.py
@@ -9,17 +9,17 @@ from .models import Song
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'congratulation/index.html')
 
 
 def show_tracklist(request):
-    template = loader.get_template('tracklist.html')
+    template = loader.get_template('congratulation/tracklist.html')
     songs = Song.objects.order_by('author')
     context = {'songs': songs}
     return HttpResponse(template.render(context, request))
 
 
 class OrderCreateView(CreateView):
-    template_name = 'order.html'
+    template_name = 'congratulation/order.html'
     form_class = OrderForm
     success_url = reverse_lazy('index')
